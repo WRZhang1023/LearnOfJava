8f5a6448f237a30a3a90429808f93b3087b5b798
weirong.zhang <weirong.zhang@beebank.com>
2016-08-30 18:40:33 +0800

    四种线程池的示例

---
+++ src/ind/fourkindthread/MyThread.java
@@ -0,0 +1,11 @@
+package ind.fourkindthread;
+
+/**
+ * Created by weirong.zhang on 16/8/30.
+ */
+public class MyThread extends Thread{
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " 正在执行。。。(注意线程的名称)");
+    }
+}
---
+++ src/ind/fourkindthread/TestSingleThreadExecutor.java
@@ -0,0 +1,89 @@
+package ind.fourkindthread;
+
+import org.junit.Test;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by weirong.zhang on 16/8/30.
+ * 四种线程池的示例,可进一步研究里面的方法
+ */
+public class TestSingleThreadExecutor {
+
+    @Test
+    public void newSingleThreadExecutor() {
+        //创建一个可重用固定线程数的线程池
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+        //创建实现了Runnable接口对象，Thread对象当然也实现了Runnable接口
+        Thread t1 = new MyThread();
+        Thread t2 = new MyThread();
+        Thread t3 = new MyThread();
+        Thread t4 = new MyThread();
+        Thread t5 = new MyThread();
+        //将线程放入池中进行执行
+        pool.execute(t1);
+        pool.execute(t2);
+        pool.execute(t3);
+        pool.execute(t4);
+        pool.execute(t5);
+        //关闭线程池
+        pool.shutdown();
+    }
+
+    @Test
+    public void newFixedThreadPool(){
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        Thread t1 = new MyThread();
+        Thread t2 = new MyThread();
+        Thread t3 = new MyThread();
+        Thread t4 = new MyThread();
+        Thread t5 = new MyThread();
+        //将线程放入池中进行执行
+        pool.execute(t1);
+        pool.execute(t2);
+        pool.execute(t3);
+        pool.execute(t4);
+        pool.execute(t5);
+        //关闭线程池
+        pool.shutdown();
+    }
+
+
+    @Test
+    public void newCachedThreadPool(){
+        ExecutorService pool = Executors.newCachedThreadPool();
+        Thread t1 = new MyThread();
+        Thread t2 = new MyThread();
+        Thread t3 = new MyThread();
+        Thread t4 = new MyThread();
+        Thread t5 = new MyThread();
+        //将线程放入池中进行执行
+        pool.execute(t1);
+        pool.execute(t2);
+        pool.execute(t3);
+        pool.execute(t4);
+        pool.execute(t5);
+        //关闭线程池
+        pool.shutdown();
+    }
+
+    @Test
+    public void newScheduleThreadPool(){
+        ExecutorService pool = Executors.newScheduledThreadPool(2);
+        Thread t1 = new MyThread();
+        Thread t2 = new MyThread();
+        Thread t3 = new MyThread();
+        Thread t4 = new MyThread();
+        Thread t5 = new MyThread();
+        //将线程放入池中进行执行
+        pool.execute(t1);
+        pool.execute(t2);
+        pool.execute(t3);
+        pool.execute(t4);
+        pool.execute(t5);
+        //关闭线程池
+        pool.shutdown();
+    }
+}
